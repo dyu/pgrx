83808959c1118c7b54c3c6fdecb2df42644ac459
dyu <david.yu.ftw@gmail.com>
2025-04-10 21:28:25 +0800

    openssl linux

--- install.sh
+++ install.sh
@@ -16,8 +16,47 @@ else
     SCRIPT_DIR=`dirname $F`
 fi
 # change pwd
-cd $SCRIPT_DIR/cargo-pgrx
+cd $SCRIPT_DIR
 
+UNAME=`uname`
+LINUX_OPENSSL_DIR="$SCRIPT_DIR/target/openssl"
+
+dl_to_dir() {
+  cd $1
+  curl -LO $2
+  cd - > /dev/null
+}
+
+export_build_flags() {
+  export OPENSSL_DIR="$1"
+  export LD_LIBRARY_PATH="$1/lib"
+}
+
+if [ "$UNAME" != 'Linux' ]; then
+echo
+elif [ -e "$LINUX_OPENSSL_DIR" ]; then
+export_build_flags $LINUX_OPENSSL_DIR
+else
+OPENSSL_VERSION="1.1.1w"
+OPENSSL_SUFFIX=`echo "$OPENSSL_VERSION" | tr . _`
+OPENSSL_PARENT_DIR="$PWD/target"
+OPENSSL_EXTRACT_DIR="$OPENSSL_PARENT_DIR/openssl-$OPENSSL_VERSION"
+OPENSSL_URL="https://github.com/openssl/openssl/releases/download/OpenSSL_$OPENSSL_SUFFIX/openssl-$OPENSSL_VERSION.tar.gz"
+
+mkdir -p target && cd target
+
+[ -e "$OPENSSL_EXTRACT_DIR.tar.gz" ] || dl_to_dir $OPENSSL_PARENT_DIR $OPENSSL_URL || exit 1
+[ -e "$OPENSSL_EXTRACT_DIR" ] || tar -xzvf $OPENSSL_EXTRACT_DIR.tar.gz -C $OPENSSL_PARENT_DIR || exit 1
+
+cd $OPENSSL_EXTRACT_DIR && \
+./config --prefix=$LINUX_OPENSSL_DIR && \
+make && \
+make install && \
+cd $SCRIPT_DIR && \
+export_build_flags $LINUX_OPENSSL_DIR
+fi
+
+cd cargo-pgrx && \
 cargo build --release && \
 cargo install --path . && \
 [ -e "$1/bin/pg_config" ] && \
